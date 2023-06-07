ec18450aa661cab1adc42b8f52ae0bb36259ae21
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-06-07 19:25:36 -0300

    feat: :sparkles: Modifying error messages in form validation

--- src/utils/schema/FormLoginSchema.ts
+++ src/utils/schema/FormLoginSchema.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const FormLoginSchema = Joi.object({
-	name: Joi.string().alphanum().min(5).max(20).trim().required(),
-	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9\\s]{3,30}$')).trim().required(),
+	name: Joi.string().alphanum().min(5).max(20).trim().required().label('UserName'),
+	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9\\s]{3,30}$')).trim().required().label('Password'),
 	passwordConfirm: Joi.not().exist(),
 });
--- src/utils/schema/FormRegisterSchema.ts
+++ src/utils/schema/FormRegisterSchema.ts
@@ -1,7 +1,10 @@
 import Joi from 'joi';
 
 export const FormRegisterSchema = Joi.object({
-	name: Joi.string().alphanum().min(5).max(20).trim().required(),
-	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9\\s]{3,30}$')).required(),
-	passwordConfirm: Joi.ref('password'),
+	name: Joi.string().alphanum().min(5).max(20).trim().required().label('UserName'),
+	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9\\s]{3,30}$')).required().label('Password'),
+	passwordConfirm: Joi.any().valid(Joi.ref('password')).required().messages({
+		'any.required': `Confirm Password is a required field.`,
+		'any.only': 'Password must match',
+	}),
 }).with('password', 'passwordConfirm');
